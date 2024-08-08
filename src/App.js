39a300d388857f8e9fc6bdc1b49093d23d300ba3
Yogeshkodes <yogesh.cr712@gmail.com>
2024-08-08 12:50:40 +0530

    frontend a lot

--- src/App.js
+++ src/App.js
@@ -9,8 +9,18 @@ import ForgotPassword from "./Pages/ForgotPassword";
 import UpdatePassword from "./Pages/UpdatePassword";
 import VerifyEmail from "./Pages/VerifyEmail";
 import About from "./Pages/About";
+import Contact from "./Pages/Contact";
+import MyProfile from "./Component/Core/Dashboard/MyProfile";
+import Dashboard from "./Pages/Dashboard";
+import PrivateRoute from "./Component/Core/Auth/PrivateRoute";
+import { ACCOUNT_TYPE } from "./utils/constants";
+import { useSelector } from "react-redux";
+import EnrolledCourses from "./Component/Core/Dashboard/EnrolledCourses";
+import Settings from "./Component/Core/Dashboard/Settings";
+import Cart from "./Component/Core/Dashboard/Cart";
 
 function App() {
+  const { user } = useSelector((state) => state.profile);
   return (
     <div className="w-screen min-h-screen bg-richblack-900 flex flex-col font-inter">
       <Navbar />
@@ -60,6 +70,27 @@ function App() {
         />
 
         <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+
+        <Route
+          element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          }
+        >
+          <Route path="dashboard/my-profile" element={<MyProfile />} />
+          <Route path="dashboard/settings" element={<Settings />} />
+          {user?.accountType === ACCOUNT_TYPE.STUDENT && (
+            <>
+              <Route path="dashboard/cart" element={<Cart />} />
+              <Route
+                path="dashboard/enrolled-courses"
+                element={<EnrolledCourses />}
+              />
+            </>
+          )}
+        </Route>
       </Routes>
     </div>
   );
